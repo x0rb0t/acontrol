77c950cdfef885f7a2da9bc5fb62f4be23354767
Андрей Александрович <x0rb0t@gmail.com>
2014-05-07 16:19:49 +0400

    test for task in task

--- main.cpp
+++ main.cpp
@@ -19,22 +19,31 @@ int main()
     }
     return sum;
   };
-  std::vector<decltype(act::make_task(lmdb, big.begin(), big.end()))> tasks;
+  typedef decltype(act::make_task(lmdb, big.begin(), big.end())) task_type;
+  std::vector<task_type> tasks;
   for (int i =0; i < 100; ++i)
   {
-    auto task = act::make_task(lmdb, big.begin(), big.end());
+    task_type task = act::make_task(lmdb, big.begin(), big.end());
     control << task;
     tasks.push_back(task);
   } 
   cout << "wait! " << endl;
   //control << act::control::sync();
-  double s = 0;
-  for (auto x: tasks)
+
+  auto end_task = act::make_task([](std::vector<task_type>::iterator begin, std::vector<task_type>::iterator end)
   {
-    s += x->get();
-    cout << s << endl;
+    double sum = 0;
+    for (auto i = begin; i != end; ++i)
+    {
+      sum+=(*i)->get();
+    }
+    return sum;
   }
-  cout << "Hello World: " << s << endl;
+  , tasks.begin(), tasks.end());
+  control << end_task;
+  cout << "wait2 " << endl;
+  control << act::control::sync();
+  cout << "Hello World: " << end_task->get() << endl;
 
   return 0;
 }
