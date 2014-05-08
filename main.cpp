2713fac7d849674d719d7f106cc7b1c764d88e9c
Андрей Александрович <x0rb0t@gmail.com>
2014-05-08 10:59:15 +0400

    Сравнение с async

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <acontrol.hpp>
+
+#include <future>
 using namespace std;
 int main()
 {
-  act::control control(16);
+  act::control control(6);
   //std::atomic<int> cnt = {0};
   std::vector<double> big;
   for (int i = 0; i < 100000000; ++i)
@@ -21,15 +23,14 @@ int main()
   };
   typedef decltype(act::make_task(lmdb, big.begin(), big.end())) task_type;
   std::vector<task_type> tasks;
+
+  auto t1 = std::chrono::high_resolution_clock::now();
   for (int i =0; i < 100; ++i)
   {
     task_type task = act::make_task(lmdb, big.begin(), big.end());
     control << task;
     tasks.push_back(task);
-  } 
-  cout << "wait! " << endl;
-  //control << act::control::sync();
-
+  }
   auto end_task = act::make_task([](std::vector<task_type>::iterator begin, std::vector<task_type>::iterator end)
   {
     double sum = 0;
@@ -40,10 +41,33 @@ int main()
     return sum;
   }
   , tasks.begin(), tasks.end());
-  control << end_task;
-  cout << "wait2 " << endl;
+  control << end_task; 
   control << act::control::sync();
-  cout << "Hello World: " << end_task->get() << endl;
+  double val = end_task->get();
+  auto t2 = std::chrono::high_resolution_clock::now();
+  cout << "act: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count() << " " << val << endl;
+
+  t1 = std::chrono::high_resolution_clock::now();
+  typedef decltype(std::async(std::launch::async, lmdb, big.begin(), big.end())) async_type;
+  std::vector<async_type> asyncs;
+  for (int i =0; i < 100; ++i)
+  {
+    //async_type handle = ;
+    asyncs.push_back(std::move(std::async(std::launch::async, lmdb, big.begin(), big.end())));
+  }
+  auto end_async = std::async(std::launch::async, [](std::vector<async_type>::iterator begin, std::vector<async_type>::iterator end)
+  {
+    double sum = 0;
+    for (auto i = begin; i != end; ++i)
+    {
+      sum+=(*i).get();
+    }
+    return sum;
+  }
+  , asyncs.begin(), asyncs.end());
+  val = end_async.get();
+  t2 = std::chrono::high_resolution_clock::now();
+  cout << "async: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count() << " " << val << endl;
 
   return 0;
 }
